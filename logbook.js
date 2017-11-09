5af84cf2a32208616968fefad269d7d6362637f6
Julieta Adlaon <julietamelendrez0800@gmail.com>
2017-11-09 12:49:21 +0800

    updated

--- logbook.js
+++ logbook.js
@@ -22,4 +22,18 @@ GetClock();
 setInterval(GetClock,1000);
 }
 
+function isNumberKey(evt)
+{
+  var charCode = (evt.which) ? evt.which : event.keyCode;
+ console.log(charCode);
+    if (charCode != 46 && charCode != 45 && charCode > 31
+    && (charCode < 48 || charCode > 57))
+     return false;
+
+  return true;
+}
+
+
+
+
 
